69d015bbdd4d1d4544628bd8b98684466fa8b01e
Mateusz <rex89m@wp.pl>
2021-10-17 16:45:55 +0200

    start

---
+++ src/main/java/pl/rex89m/mdrop/MDrop.java
@@ -0,0 +1,17 @@
+package pl.rex89m.mdrop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MDrop extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
